6264754bcf6718ebc3d0c320ca6dd7c6a2d12c7f
Daniel Cazzulino <daniel@cazzulino.com>
2021-06-24 10:15:17 -0300

    Clean usings, add missing file headers

--- src/TableStorage/IQueryableExtensions.cs
+++ src/TableStorage/IQueryableExtensions.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace Devlooped
 {
--- src/Tests/DocumentRepositoryTests.cs
+++ src/Tests/DocumentRepositoryTests.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Threading.Tasks;
 using MessagePack;
 using Microsoft.Azure.Cosmos.Table;
