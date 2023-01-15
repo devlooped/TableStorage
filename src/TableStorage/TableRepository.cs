88538cca00e7e8ec946c909aa25bfa429b263709
Daniel Cazzulino <daniel@cazzulino.com>
2023-01-15 19:03:41 -0300

    Add TableConnection overloads for TableRepository factory methods
    Closes #161

--- src/TableStorage/TableRepository.cs
+++ src/TableStorage/TableRepository.cs
@@ -30,6 +30,17 @@ namespace Devlooped
             => new TableEntityRepository(storageAccount, tableName) { UpdateMode = updateMode };
 
         /// <summary>
+        /// Creates an <see cref="ITableRepository{TableEntity}"/> repository.
+        /// </summary>
+        /// <param name="tableConnection">The storage account and table to use.</param>
+        /// <param name="updateMode">Update mode for existing entities. Defaults to <see cref="TableUpdateMode.Merge"/>.</param>
+        /// <returns>The new <see cref="ITableRepository{ITableEntity}"/>.</returns>
+        public static ITableRepository<TableEntity> Create(
+            TableConnection tableConnection,
+            TableUpdateMode updateMode = TableUpdateMode.Merge)
+            => new TableEntityRepository(tableConnection) { UpdateMode = updateMode };
+
+        /// <summary>
         /// Creates an <see cref="ITableRepository{T}"/> for the given entity type 
         /// <typeparamref name="T"/>, using the <typeparamref name="T"/> <c>Name</c> as 
         /// the table name.
@@ -44,6 +55,21 @@ namespace Devlooped
             Expression<Func<T, string>> partitionKey,
             Expression<Func<T, string>> rowKey) where T : class
             => Create<T>(storageAccount, typeof(T).Name, partitionKey, rowKey, TableUpdateMode.Merge);
+
+        /// <summary>
+        /// Creates an <see cref="ITableRepository{T}"/> for the given entity type 
+        /// <typeparamref name="T"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of entity that the repository will manage.</typeparam>
+        /// <param name="tableConnection">The storage account and table to use.</param>
+        /// <param name="partitionKey">Function to retrieve the partition key for a given entity.</param>
+        /// <param name="rowKey">Function to retrieve the row key for a given entity.</param>
+        /// <returns>The new <see cref="ITableRepository{T}"/>.</returns>
+        public static ITableRepository<T> Create<T>(
+            TableConnection tableConnection,
+            Expression<Func<T, string>> partitionKey,
+            Expression<Func<T, string>> rowKey) where T : class
+            => Create<T>(tableConnection, partitionKey, rowKey, TableUpdateMode.Merge);
 
         /// <summary>
         /// Creates an <see cref="ITableRepository{T}"/> for the given entity type 
@@ -65,17 +91,35 @@ namespace Devlooped
             Expression<Func<T, string>>? partitionKey = null,
             Expression<Func<T, string>>? rowKey = null,
             TableUpdateMode updateMode = TableUpdateMode.Merge) where T : class
+            => Create<T>(new TableConnection(storageAccount, tableName ?? GetDefaultTableName<T>()), partitionKey, rowKey, updateMode);
+
+        /// <summary>
+        /// Creates an <see cref="ITableRepository{T}"/> for the given entity type 
+        /// <typeparamref name="T"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of entity that the repository will manage.</typeparam>
+        /// <param name="tableConnection">The storage account and table to use.</param>
+        /// <param name="partitionKey">Optional function to retrieve the partition key for a given entity. 
+        /// If not provided, the class will need a property annotated with <see cref="PartitionKeyAttribute"/>.</param>
+        /// <param name="rowKey">Optional function to retrieve the row key for a given entity. 
+        /// If not provided, the class will need a property annotated with <see cref="RowKeyAttribute"/>.</param>
+        /// <param name="updateMode">Update mode for existing entities. Defaults to <see cref="TableUpdateMode.Merge"/>.</param>
+        /// <returns>The new <see cref="ITableRepository{T}"/>.</returns>
+        public static ITableRepository<T> Create<T>(
+            TableConnection tableConnection,
+            Expression<Func<T, string>>? partitionKey = null,
+            Expression<Func<T, string>>? rowKey = null,
+            TableUpdateMode updateMode = TableUpdateMode.Merge) where T : class
         {
-            tableName ??= GetDefaultTableName<T>();
             partitionKey ??= PartitionKeyAttribute.CreateAccessor<T>();
             rowKey ??= RowKeyAttribute.CreateAccessor<T>();
 
-            return new TableRepository<T>(storageAccount, tableName, partitionKey, rowKey)
+            return new TableRepository<T>(tableConnection, partitionKey, rowKey)
             {
                 UpdateMode = updateMode
             };
         }
-
+        
         /// <summary>
         /// Gets a default table name for entities of type <typeparamref name="T"/>. Will be the 
         /// <see cref="TableAttribute.Name"/> if the attribute is present, or the type name otherwise.
