01019ed2d04252536fa0b36cee7a43cc5c43ab6c
Daniel Cazzulino <daniel@cazzulino.com>
2023-03-28 18:15:29 -0300

    Add SponsorLink to ensure ongoing development and support

---
+++ src/TableStorage.CodeAnalysis/SponsorLinker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Devlooped.TableStorage;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic, LanguageNames.FSharp)]
+class SponsorLinker : SponsorLink
+{
+    public SponsorLinker() : base(SponsorLinkSettings.Create(
+        "devlooped", "Devlooped.TableStorage",
+        diagnosticsIdPrefix: "DTS",
+        version: new Version(ThisAssembly.Info.Version).ToString(3)
+#if DEBUG
+        , quietDays: 0
+#endif
+        ))
+    { }
+}
